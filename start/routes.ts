321a5f03938059119a08e7e763a9df95248a16f2
Wevelly Felipe <wevellysantos@gmail.com>
2024-03-05 10:31:15 -0300

    init

---
+++ start/routes.ts
@@ -0,0 +1,16 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.get('/wppwebhooks', async () => {
+  return {
+    hello: 'world',
+  }
+})
